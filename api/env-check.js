65d034e5e00cbc2993972ca05a5b5dbeabba8f9a
rjgheen <rjgheen@duewiseai.com>
2025-10-20 22:44:35 -0600

    Update env-check.js

--- api/env-check.js
+++ api/env-check.js
@@ -1,12 +1,20 @@
+// api/env-check.js
 export default function handler(_req, res) {
-  const keys = [
-    'STRIPE_SECRET',
-    'STRIPE_WEBHOOK_SECRET',
-    'DROPBOX_CLIENT_ID',
-    'DROPBOX_CLIENT_SECRET',
-    'DROPBOX_REFRESH_TOKEN',
-    'CHECKOUT_LINK_TOKEN',
-    'ALLOW_TEST_AUTOGEN',
+  const need = [
+    'STRIPE_SECRET', 'STRIPE_WEBHOOK_SECRET',
+    'DROPBOX_CLIENT_ID', 'DROPBOX_CLIENT_SECRET', 'DROPBOX_REFRESH_TOKEN',
+    'GMAIL_CLIENT_ID', 'GMAIL_CLIENT_SECRET', 'GMAIL_REFRESH_TOKEN',
+    'GMAIL_SENDER',
+    'KV_REST_API_URL', 'KV_REST_API_TOKEN'
   ];
-  res.status(200).json(Object.fromEntries(keys.map(k => [k, !!process.env[k]])));
+  const out = {};
+  for (const k of need) out[k] = !!process.env[k];
+  out.SUCCESS_URL = !!process.env.SUCCESS_URL;
+  out.CANCEL_URL = !!process.env.CANCEL_URL;
+  out.OPS_EMAIL = !!process.env.OPS_EMAIL;
+  out.WIX_ORDERS_ENDPOINT = !!process.env.WIX_ORDERS_ENDPOINT;
+  out.WIX_SHARED_SECRET = !!process.env.WIX_SHARED_SECRET;
+  out.CRON_TOKEN = !!process.env.CRON_TOKEN;
+  out.ADMIN_TOKEN = !!process.env.ADMIN_TOKEN;
+  res.status(200).json(out);
 }
